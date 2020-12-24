4c42e94a3433f0e3662adfbb2dfe290b73481edd
juth60252 <74052135+juth60252@users.noreply.github.com>
2020-12-24 12:01:26 +0100

    toutes les classes + fenetre

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = new Pane();
+        Scene scene = new Scene(root,650,650);
+
+        primaryStage.setTitle("Mister Jack's Pocket");
+        //primaryStage.getIcons().add(new Image("file:Pictures/icon.png"));
+        primaryStage.setResizable(false);
+        primaryStage.setWidth(650);
+        primaryStage.setHeight(650);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/MisterJack.java
@@ -0,0 +1,2 @@
+public class MisterJack {
+}
