d159a2a7013ee346035bade5c31ee00d05e7b1f0
juth60252 <74052135+juth60252@users.noreply.github.com>
2020-12-26 13:58:14 +0100

    Update CarteAlibi.java

--- src/CarteAlibi.java
+++ src/CarteAlibi.java
@@ -1,2 +1,10 @@
 public class CarteAlibi {
+
+    String nom;
+    int nbSabliers;
+
+    public CarteAlibi(String nom, int nbSabliers) {
+        this.nom = nom;
+        this.nbSabliers = nbSabliers;
+    }
 }
