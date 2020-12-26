12257bc402e61a0d4d1a6d4da4c42d7fd7c7540d
juth60252 <74052135+juth60252@users.noreply.github.com>
2020-12-26 22:24:24 +0100

    Menu

--- src/App.java
+++ src/App.java
@@ -1,8 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class App extends Application {
 
@@ -15,11 +21,13 @@ public class App extends Application {
         Pane root = new Pane();
         Scene scene = new Scene(root,650,650);
 
+        menu(primaryStage, scene, root);
+
         primaryStage.setTitle("Mister Jack's Pocket");
         primaryStage.getIcons().add(new Image("file:images/JetonsDetective/Holmes.png"));
         primaryStage.setResizable(false);
-        primaryStage.setWidth(650);
-        primaryStage.setHeight(650);
+        primaryStage.setWidth(665);
+        primaryStage.setHeight(680);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -28,8 +36,81 @@ public class App extends Application {
 
         launch(args);
     }
-    public void menu(){
+    public void menu(Stage stage,Scene scene,Pane root) throws FileNotFoundException {
         //Menu du jeu
+
+        /*    ----------------------------------
+                Gestion du menu avec le clavier
+              ---------------------------------- */
+
+        //Atomic integer nécessaire pour le passer dans lambda exp
+        AtomicInteger count = new AtomicInteger(1); //compteur qui indique au programme où se trouve le curseur
+
+        loadImage(root,new FileInputStream("images\\Menu\\Menu1.png"));//Le curseur est sur play par défaut
+        scene.setOnKeyPressed(e -> { //scene receptives aux évenements clavier
+            if(e.getCode().equals(KeyCode.DOWN)){ // si flèche du bas
+                try {
+                    loadImage(root,new FileInputStream("images\\Menu\\Menu2.png")); // curseur sur Quit
+                    count.set(2);
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+            }
+            if(e.getCode().equals(KeyCode.UP)){ // si flèche du haut
+                try {
+                    loadImage(root,new FileInputStream("images\\Menu\\Menu1.png")); // curseur sur play
+                    count.set(1);
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+            }
+            if(e.getCode().equals(KeyCode.ENTER) && count.get()==1){ // si Entrer sur play
+                    //on lance la partie
+            }
+            if(e.getCode().equals(KeyCode.ENTER) && count.get()==2){ // si Entrer sur Quit
+                stage.close();
+            }
+        });
+
+       /*     ----------------------------------
+                Gestion du menu avec la souris
+              ---------------------------------- */
+
+        scene.setOnMouseMoved(e -> {
+            if(e.getX() > 240 && e.getX() < 395 && e.getY() > 140 && e.getY() < 285){
+                try {
+                    loadImage(root,new FileInputStream("images\\Menu\\Menu1.png")); // curseur sur play
+                    count.set(1);
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+            }
+            if(e.getX() > 240 && e.getX() < 395 && e.getY() > 350 && e.getY() < 440){
+                try {
+                    loadImage(root,new FileInputStream("images\\Menu\\Menu2.png")); // curseur sur Quit
+                    count.set(2);
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+            }
+        });
+        scene.setOnMouseClicked(e -> {
+            if(e.getX() > 240 && e.getX() < 395 && e.getY() > 140 && e.getY() < 285){
+                //on lance la partie
+            }
+            if(e.getX() > 240 && e.getX() < 395 && e.getY() > 350 && e.getY() < 440){
+                stage.close();
+            }
+        });
+    }
+
+    public static void loadImage(Pane root, FileInputStream inputstream) throws FileNotFoundException {
+        //Charge l'image du fichier inputstream dans la fenetre
+
+        Image img = new Image(inputstream);
+        ImageView imageView = new ImageView(img);
+        root.getChildren().addAll(imageView);
+
     }
     public void jouer(){
         //déroulement de la partie
