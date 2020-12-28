6a3565550cba50cc31c2f161baac36217908eb1a
Flumxz <74055973+Flumxz@users.noreply.github.com>
2020-12-28 01:22:21 +0100

    temp

--- src/Partie.java
+++ src/Partie.java
@@ -39,17 +39,6 @@ public class Partie extends Application {
     }
 
     public static void main(String[] args) {
-        plateau.initPlateau();
-        plateau.etatDePartie();
-
-        /*
-        plateau.echangerDistrict(1,3);
-        plateau.rotationDistrict(4,2);
-        plateau.rotationDistrict(6,1);
-        plateau.retournerDistrict(8);
-        plateau.etatDePartie();
-         */
-
         launch(args);
     }
     public void menu(Stage stage,Scene scene,Pane root) throws FileNotFoundException {
@@ -197,5 +186,27 @@ public class Partie extends Application {
 
     public void jouer(){
         //déroulement de la partie
+
+        //initialisation du plateau
+        plateau.initPlateau();
+        plateau.etatDePartie();
+
+        /* modifications de districts
+        plateau.echangerDistrict(1,3);
+        plateau.rotationDistrict(6,1);
+        plateau.retournerDistrict(8);
+        plateau.etatDePartie();
+         */
+
+        /* test de visibilité
+        plateau.districts.get(0).orientation=1;
+        plateau.districts.get(2).orientation=1;
+        plateau.districts.get(7).orientation=2;
+        plateau.Holmes.position=1;
+        plateau.Watson.position=2;
+        plateau.Toby.position=3;
+        System.out.println(plateau.isJackVisible());
+        plateau.etatDePartie();
+        */
     }
 }
